86127c9f3105421c343e4eb2a3ad84964cb0d6d2
Brian Peterson <bpeterson1579@gmail.com>
2021-12-07 14:41:12 -0700

    Add more styling to homepage

--- src/Card.js
+++ src/Card.js
@@ -3,11 +3,11 @@ import './Card.css';
 
 const Card = ({poster, title, releaseDate, id, rating, showSingleMovie}) => {
   return (
-    <div onClick={() => showSingleMovie(id)}>
+    <div className='movie' onClick={() => showSingleMovie(id)}>
       <img className='all-movie-posters' src={poster} alt='movie poster'/>
-      <p>{title}</p>
-      <p>Release Date: {releaseDate}</p>
-      <p>User Rating: {rating.toPrecision(2)}/10</p>
+      <p className='title'>{title}</p>
+      <p className='release'>Release Date: {releaseDate}</p>
+      <p className='rating'>User Rating: {rating.toPrecision(2)}/10</p>
     </div>
   );
 };
